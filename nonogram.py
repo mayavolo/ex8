15da3bde964b07849cda6b43f1644e89a8351323
Ronen <ronen4822@gmail.com>
2019-12-21 17:35:07 +0200

    conclude works!!! with double checks of total blacks and stays with '?'  for difficult board

--- nonogram.py
+++ nonogram.py
@@ -1,4 +1,14 @@
-def add_allowed_row(allowed_rows, new_rows):
+def verify_blacks(new_row, total_black):
+    blacks = 0
+    for i in range(len(new_row)):
+        if new_row[i] == 1:
+            blacks += 1
+    if blacks > total_black:
+        return False
+    return True
+
+
+def add_allowed_row(allowed_rows, new_rows, total_black):
     for k in range(len(new_rows)):
         already_there = False
         for j in range(len(allowed_rows)):
@@ -6,7 +16,8 @@ def add_allowed_row(allowed_rows, new_rows):
                 already_there = True
                 continue
         if not already_there:
-            allowed_rows.append(new_rows[k])
+            if verify_blacks(new_rows[k], total_black):
+                allowed_rows.append(new_rows[k])
 
     return allowed_rows
 
@@ -15,10 +26,11 @@ def get_row_variations(row, blocks):
     if len(blocks) == 0 or len(row) == 0:
         return [paint_white(row, 0)]
 
-    return get_row_variations_inner(row, blocks, 0, True)
+    total_black = sum_black(blocks)
+    return get_row_variations_inner(row, blocks, 0, True, total_black)
 
 
-def get_row_variations_inner(row: list, blocks: list, index, is_clean):
+def get_row_variations_inner(row: list, blocks: list, index, is_clean, total_black):
     # print(f"row: {row}, blocks: {blocks}, index: {index}, is_clean: {is_clean}")
     row_copy = row.copy()
     if len(blocks) == 1 and blocks[0] == 0 and index <= len(row):
@@ -41,33 +53,33 @@ def get_row_variations_inner(row: list, blocks: list, index, is_clean):
     if row_copy[index] == 1:
         blocks_copy = blocks.copy()
         blocks_copy[0] -= 1
-        inner_rows = get_row_variations_inner(row_copy, blocks_copy, index + 1, False)
+        inner_rows = get_row_variations_inner(row_copy, blocks_copy, index + 1, False, total_black)
         if inner_rows:
-            allowed_rows = add_allowed_row(allowed_rows, inner_rows)
+            allowed_rows = add_allowed_row(allowed_rows, inner_rows, total_black)
 
     if row_copy[index] == -1:
         if is_clean:
             blocks_copy = blocks.copy()
             row_inner_copy = row_copy.copy()
             row_inner_copy[index] = 0
-            inner_rows = get_row_variations_inner(row_inner_copy, blocks_copy, index + 1, is_clean)
+            inner_rows = get_row_variations_inner(row_inner_copy, blocks_copy, index + 1, is_clean, total_black)
             if inner_rows:
-                allowed_rows = add_allowed_row(allowed_rows, inner_rows)
+                allowed_rows = add_allowed_row(allowed_rows, inner_rows, total_black)
 
         if not must_be_white:
             row_inner_copy = row_copy.copy()
             row_inner_copy[index] = 1
             blocks_copy = blocks.copy()
             blocks_copy[0] -= 1
-            inner_rows = get_row_variations_inner(row_inner_copy, blocks_copy, index + 1, False)
+            inner_rows = get_row_variations_inner(row_inner_copy, blocks_copy, index + 1, False, total_black)
             if inner_rows:
-                allowed_rows = add_allowed_row(allowed_rows, inner_rows)
+                allowed_rows = add_allowed_row(allowed_rows, inner_rows, total_black)
 
     if row_copy[index] == 0:
         blocks_copy = blocks.copy()
-        inner_rows = get_row_variations_inner(row_copy, blocks_copy, index + 1, is_clean)
+        inner_rows = get_row_variations_inner(row_copy, blocks_copy, index + 1, is_clean, total_black)
         if inner_rows:
-            allowed_rows = add_allowed_row(allowed_rows, inner_rows)
+            allowed_rows = add_allowed_row(allowed_rows, inner_rows, total_black)
 
     return allowed_rows
 
@@ -115,13 +127,23 @@ def make_columns_into_rows(columns):
 
 
 def conclude_constrains_from_rows_and_columns(sorted_columns_board, sorted_rows_board, board):
+    conclusive = True
+    something_changed = False
     for row in range(len(sorted_rows_board)):
         for col in range(len(sorted_rows_board[0])):
+            prev = board[row][col]
             if sorted_rows_board[row][col] == -1:
                 board[row][col] = sorted_columns_board[row][col]
             else:
                 board[row][col] = sorted_rows_board[row][col]
-    return board
+            if board[row][col] == -1:
+                conclusive = False
+            if prev != board[row][col]:
+                something_changed = True
+
+    if not something_changed:
+        conclusive = True
+    return conclusive
 
 
 # def conclude_constrains_from_rows_and_columns(sorted_columns_board, sorted_rows_board):
@@ -132,36 +154,46 @@ def conclude_constrains_from_rows_and_columns(sorted_columns_board, sorted_rows_
 #                 final_board[row][col] = sorted_columns_board[row][col]
 #     return final_board
 
+def sum_black(blocks):
+    sum = 0;
+    for i in range(len(blocks)):
+        sum += blocks[i]
+    return sum
+
 
 def conclude_from_constraints(board, constraints):
     if len(board) == 0:
         return None
 
-    sorted_rows_board = board.copy()
-    all_columns = []
+    conclusive = False
 
-    for row_index in range(len(board)):
-        starting_rows = get_row_variations(board[row_index], constraints[0][row_index])
-        semi_final_row = get_intersection_row(starting_rows)
-        sorted_rows_board[row_index] = semi_final_row
+    while not conclusive:
 
-    for col_index in range(len(board[0])):
-        col = get_a_column(board, col_index)
-        starting_columns = get_row_variations(col, constraints[1][col_index])
-        semi_final_col = get_intersection_row(starting_columns)
-        all_columns.append(semi_final_col)
-    sorted_col_board = make_columns_into_rows(all_columns)
+        sorted_rows_board = board.copy()
+        all_columns = []
+        for row_index in range(len(board)):
+            starting_rows = get_row_variations(board[row_index], constraints[0][row_index])
+            semi_final_row = get_intersection_row(starting_rows)
+            sorted_rows_board[row_index] = semi_final_row
 
-    conclude_constrains_from_rows_and_columns(sorted_col_board, sorted_rows_board, board)
-    print(f'the board is: {board}')
+        for col_index in range(len(board[0])):
+            col = get_a_column(board, col_index)
+            starting_columns = get_row_variations(col, constraints[1][col_index])
+            semi_final_col = get_intersection_row(starting_columns)
+            all_columns.append(semi_final_col)
+        sorted_col_board = make_columns_into_rows(all_columns)
+
+        conclusive = conclude_constrains_from_rows_and_columns(sorted_col_board, sorted_rows_board, board)
+        print(f'the board is: {board}')
+
     return None
-
 
 # conclude_from_constraints([[-1, -1, -1, -1, -1, -1]], [[[2, 1]], [[], [1], [1], [], [1], []]])
 # conclude_constrains_from_rows_and_columns(
 #     [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]],
-#     [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]])
-res = get_row_variations([-1, -1, -1, -1, -1], [1,3])
-print(res)
+#     )
+# res = get_row_variations([-1, 1, 1, 1, 0], [4])
+# print(res)
+# print(get_intersection_row(res))
 # for i in range(len(res)):
 #    print(res[i])
